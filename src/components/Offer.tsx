577edbaad0f0d69fb78d2b65a9cb09be5178150b
unknown <akbiyik.ilker7@gmail.com>
2022-05-20 17:31:05 +0200

    minor changes

--- src/components/Offer.tsx
+++ src/components/Offer.tsx
@@ -1,4 +1,4 @@
-import React, { Dispatch, SetStateAction, useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styled from 'styled-components'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faRocket,  faCode, faRobot, faGift } from '@fortawesome/free-solid-svg-icons';
