6e9b32371db0dfc2463b3857a9b5e445efd728fc
unknown <akbiyik.ilker7@gmail.com>
2022-05-24 17:42:05 +0200

    working on navigation

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,24 +1,39 @@
 import * as React from 'react';
-import Map from 'react-map-gl';
-import styled from 'styled-components';
+import ReactMapGL, {
+  Marker, 
+  AttributionControl, 
+  FullscreenControl, 
+  GeolocateControl, 
+  NavigationControl, 
+  Popup 
+} from 'react-map-gl';
+import pin from '../assets/pin.png'
 
 function MapComponent() {
-  return <StyledMap
+  return <ReactMapGL
     initialViewState={{
-      longitude: -100,
-      latitude: 40,
-      zoom: 3.5
+      latitude: 48.87,
+      longitude: 2.3,
+      zoom: 5.5
     }}
     mapStyle="mapbox://styles/mapbox/streets-v9"
+    style={{
+      height:"600px", 
+      width: "100%",
+    }}
+    attributionControl={false}
     mapboxAccessToken={process.env.REACT_APP_MAPBOX_ACCESS_TOKEN}
-  />;
+  > 
+    <GeolocateControl
+      positionOptions={{enableHighAccuracy: true}}
+      trackUserLocation={false}
+    />
+    <FullscreenControl />
+    <Marker longitude={2.3} latitude={48.87} anchor="bottom" >
+      <img style={{width:"24px", height:"24px",top:"-12px", bottom: "-12px"}} src={pin} alt="pin" />
+    </Marker>
+    <NavigationControl />
+  </ReactMapGL>;
 }
 
 export default MapComponent
-
-
-const StyledMap = styled(Map)`
-  width: 100%;
-  min-height: 500px;
-
-`
